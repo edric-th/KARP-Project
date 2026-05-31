eef01c326b5614c394259e1ae739efbe66792331
Prawnis <pranishthapa574@gmail.com>
2026-05-31 11:26:07 +0545

    fixing frontend

--- frontend/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:frontend/main.dart';
-
-void main() {
-  testWidgets('App launches without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(const MeroPaloApp());
-    expect(find.text('Mero Palo'), findsAny);
-  });
-}
